087feff2a1e3b99060bd3164ab549890930a8286
Faizan <fasa@mapspeople.com>
2023-03-29 19:49:23 +0200

    basic view

--- MapsIndoorsTemplate-iOS-v4/ContentView.swift
+++ MapsIndoorsTemplate-iOS-v4/ContentView.swift
@@ -1,15 +1,24 @@
 //  ContentView.swift
 import SwiftUI
+import MapsIndoorsCore
+import MapsIndoorsGoogleMaps
+import GoogleMaps
 
 struct ContentView: View {
+    @State private var searchResult: [MPLocation]?
+    @State private var origin: MPLocation?
+    @State private var mpMapControl: MPMapControl?
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            ZStack {
+                MapView(searchResult: $searchResult, origin: $origin, mpMapControl: $mpMapControl)
+                    .edgesIgnoringSafeArea(.all)
+
+                // Add your search bar and other UI components here
+            }
+            .navigationBarTitle("MapsIndoors v4 SwiftUI", displayMode: .inline)
         }
-        .padding()
     }
 }
 
