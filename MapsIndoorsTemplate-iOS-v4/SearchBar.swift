484d87d15ffe15e2810e1042d7d13a7a761f31d3
fasaMaps <fasa@mapspeople.com>
2023-10-18 18:00:50 +0200

    search bar made pretty

--- MapsIndoorsTemplate-iOS-v4/SearchBar.swift
+++ MapsIndoorsTemplate-iOS-v4/SearchBar.swift
@@ -22,6 +22,39 @@ struct SearchBar: UIViewRepresentable {
     func makeUIView(context: UIViewRepresentableContext<SearchBar>) -> UISearchBar {
         let searchBar = UISearchBar(frame: .zero)
         searchBar.delegate = context.coordinator
+        
+        // Set the background to be transparent
+        searchBar.barTintColor = UIColor.clear
+        searchBar.backgroundColor = UIColor.clear
+        searchBar.isTranslucent = true
+        searchBar.setBackgroundImage(UIImage(), for: .any, barMetrics: .default)
+        searchBar.searchBarStyle = .minimal
+        
+        // Customize the appearance of the text field inside the search bar
+        if let textField = searchBar.value(forKey: "searchField") as? UITextField {
+            textField.backgroundColor = UIColor.white
+            textField.layer.cornerRadius = 10.0
+            textField.clipsToBounds = true
+            textField.textColor = UIColor.black
+            textField.font = UIFont.systemFont(ofSize: 16)
+            
+            // Set a placeholder text
+            textField.attributedPlaceholder = NSAttributedString(string: "Search for buildings or locations...",
+                                                                 attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray])
+            
+            // Change the color of the search icon
+            let searchIcon = textField.leftView as? UIImageView
+            searchIcon?.image = searchIcon?.image?.withRenderingMode(.alwaysTemplate)
+            searchIcon?.tintColor = UIColor.black
+            
+            // Make the text field's edges rounded
+            textField.layer.cornerRadius = 15.0
+            textField.clipsToBounds = true
+        }
+        
+        // Set the keyboard appearance
+        searchBar.keyboardAppearance = .light
+        
         return searchBar
     }
 
